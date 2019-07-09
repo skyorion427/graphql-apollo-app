922cf755c35fad1a84e879bfcc93679229ba2050
skyorion427 <skyorion427@gmail.com>
2019-07-08 22:15:02 -0400

    Update CircleCI config and Add delivery time

--- server/index.js
+++ server/index.js
@@ -22,6 +22,12 @@ app.set('port', config.PORT);
 
 server.applyMiddleware({ app });
 
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+
 app.listen(app.get('port'), () => {
   console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
 });
